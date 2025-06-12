44d2a79c74b8892b5c7fb053e94f50d27dc92031
oscartorres <38195378+oscartorres-10@users.noreply.github.com>
2025-06-12 11:22:12 -0300

    chore: update favicons

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,19 +13,17 @@ const geistMono = Geist_Mono({
   subsets: ['latin'],
 })
 
-export const EMOJI_FAVICON = {
-  icon: [
-    {
-      url: 'data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🤕</text></svg>',
-      type: 'image/svg+xml',
-    },
-  ],
-}
-
 export const metadata: Metadata = {
   title: 'Patients Data app',
   description: 'See Patients Data and more!',
-  icons: EMOJI_FAVICON,
+  icons: {
+    icon: [
+      {
+        url: 'data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>👋🏼</text></svg>',
+        type: 'image/svg+xml',
+      },
+    ],
+  },
 }
 
 export default function RootLayout({
--- src/app/patients/layout.tsx
+++ src/app/patients/layout.tsx
@@ -1,10 +1,16 @@
 import type { Metadata } from 'next'
-import { EMOJI_FAVICON } from '../layout'
 
 export const metadata: Metadata = {
   title: 'Patients Data',
   description: 'See the data from all Patients',
-  icons: EMOJI_FAVICON,
+  icons: {
+    icon: [
+      {
+        url: 'data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🤓</text></svg>',
+        type: 'image/svg+xml',
+      },
+    ],
+  },
 }
 
 export default function PatientsLayout({
